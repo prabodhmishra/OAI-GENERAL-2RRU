261aacd4e469ca3a2bbd7aaef0ae51c279ee7a24
Prabodh Mishra <pmishra@clemson.edu>
2020-07-11 17:44:02 -0400

    Remove VLAN tagging

--- profile.py
+++ profile.py
@@ -242,7 +242,7 @@ else:
     rru1.disk_image = GLOBALS.OAI_ENB_IMG
     rru1.Desire( "rf-controlled", 1 )
     connectOAI_DS(rru1, 0)
-    rru1.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r ENB"))
+    #rru1.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r ENB"))
     rru1_rue1_rf = rru1.addInterface("rue1_rf")
 
     # Add second NUC RRU node.
@@ -253,7 +253,7 @@ else:
     rru2.disk_image = GLOBALS.OAI_ENB_IMG
     rru2.Desire( "rf-controlled", 1 )
     connectOAI_DS(rru2, 0)
-    rru2.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r ENB"))
+    #rru2.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r ENB"))
     rru2_rue1_rf = rru2.addInterface("rue1_rf")
 
     # Add a NUC eNB node.
@@ -264,7 +264,7 @@ else:
     enb1.disk_image = GLOBALS.OAI_ENB_IMG
     enb1.Desire( "rf-controlled", 1 )
     connectOAI_DS(enb1, 0)
-    enb1.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r ENB"))
+    #enb1.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r ENB"))
     # enb1_rue1_rf = enb1.addInterface("rue1_rf")
 
     # Add an OTS (Nexus 5) UE
@@ -293,7 +293,7 @@ else:
     rru1link.addNode(rru1)  
     rru1link.addNode(enb1)
     rru1link.link_multiplexing = True
-    rru1link.vlan_tagging = True
+    #rru1link.vlan_tagging = True
     rru1link.best_effort = True  
     
     # Add a link connecting RRU1 and the NUC eNB.
@@ -301,7 +301,7 @@ else:
     rru2link.addNode(rru2)  
     rru2link.addNode(enb1)
     rru2link.link_multiplexing = True
-    rru2link.vlan_tagging = True
+    #rru2link.vlan_tagging = True
     rru2link.best_effort = True      
 
     # Add a link connecting the NUC eNB and the OAI EPC node.
@@ -310,13 +310,13 @@ else:
 # Add OAI EPC (HSS, MME, SPGW) node.
 epc = request.RawPC("epc")
 epc.disk_image = GLOBALS.OAI_EPC_IMG
-epc.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r EPC"))
+#epc.addService(rspec.Execute(shell="sh", command=GLOBALS.OAI_CONF_SCRIPT + " -r EPC"))
 connectOAI_DS(epc, 0)
  
 epclink.addNode(epc)
-epclink.link_multiplexing = True
-epclink.vlan_tagging = True
-epclink.best_effort = True
+#epclink.link_multiplexing = True
+#epclink.vlan_tagging = True
+#epclink.best_effort = True
 
 tour = IG.Tour()
 tour.Description(IG.Tour.MARKDOWN, tourDescription)
